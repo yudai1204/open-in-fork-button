6cbc4a7018f394285a9c17ab2b75c0daf126605b
yudai hiyama <yudai1204h@gmail.com>
2023-10-28 17:03:06 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# open-in-fork-button
+
+This is the extension to use Fork easily.
+
+# How to install
+
+VSCode -> Extensions -> ... -> Install from VSIX
+
+# How to develop
+
+```shell
+npm i
+```
+
+Open this repo by VSCode
+
+Press `F5` to debug
---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+import type { ExtensionContext } from "vscode";
+import { commands, workspace, window, StatusBarAlignment } from "vscode";
+const cp = require("child_process");
+
+export function activate(context: ExtensionContext) {
+  const myButton = window.createStatusBarItem(StatusBarAlignment.Left, 0);
+  myButton.text = "Open in Fork";
+  myButton.command = "open-in-fork-button.open";
+  myButton.show();
+
+  let disposable = commands.registerCommand("open-in-fork-button.open", () => {
+    let rootPath: string = "";
+    if (workspace.workspaceFolders !== undefined) {
+      rootPath = workspace.workspaceFolders[0].uri.path;
+    } else {
+      window.showErrorMessage(
+        "Fork error: Working folder not found, open a folder an try again"
+      );
+      return;
+    }
+    cp.exec(
+      "open -a fork " + rootPath,
+      (err: any, stdout: any, stderr: any) => {
+        if (err) {
+          window.showErrorMessage("Fork error: " + err);
+        }
+      }
+    );
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
