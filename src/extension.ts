f2bef2ee4ccd93b2fba9fcd2df271b3dca5d913c
yudai hiyama <yudai1204h@gmail.com>
2023-10-29 13:42:31 +0900

    Win対応

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,20 @@
 import type { ExtensionContext } from "vscode";
 import { commands, workspace, window, StatusBarAlignment } from "vscode";
 const cp = require("child_process");
+const os = require("os");
+const path = require('path');
+
+const execFork = (command: string) => {
+  cp.exec(
+      command,
+      (err: any, stdout: any, stderr: any) => {
+        if (err) {
+          window.showErrorMessage("Fork error: " + err);
+        }
+      }
+    );
+  window.showInformationMessage(command);
+};
 
 export function activate(context: ExtensionContext) {
   const myButton = window.createStatusBarItem(StatusBarAlignment.Left, 0);
@@ -18,14 +32,33 @@ export function activate(context: ExtensionContext) {
       );
       return;
     }
-    cp.exec(
-      "open -a fork " + rootPath,
-      (err: any, stdout: any, stderr: any) => {
+    let command: string;
+    if (os.platform() === 'linux' && os.release().toLowerCase().includes('microsoft')) { 
+      // WSL
+      cp.exec('cmd.exe /c echo %USERNAME%', (err: any, stdout: any, stderr: any) => {
         if (err) {
-          window.showErrorMessage("Fork error: " + err);
+          window.showErrorMessage(
+            "WSL Error: " + err
+          );
+          return;
         }
-      }
-    );
+        const user = stdout.trim();
+        const forkPath = `C:\\Users\\${user}\\AppData\\Local\\Fork\\Fork.exe`;
+        command = `cmd.exe /c start ` + forkPath + ` ` + rootPath;
+        execFork(command);
+      });
+    } else if (os.platform() === 'darwin') {
+      // MacOS
+      command = "open -a fork "+ rootPath;
+      execFork(command);
+    } else {
+      // Windows
+      const userHome = process.env.USERPROFILE || process.env.HOME;
+      const forkPath = path.join(userHome, 'AppData', 'Local', 'Fork', 'Fork');
+      command = `start ${forkPath} ${rootPath}`;
+      execFork(command);
+    }
+    
   });
 
   context.subscriptions.push(disposable);
